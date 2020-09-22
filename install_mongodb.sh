ede3f677979a5445e7c8295d351e1c7bd641f2c0
Iliyev Telman <telman.devops@gmail.com>
2020-09-22 10:58:47 +0600

    Fix build issue

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-apt-get update
-#sudo apt-get install -y apt-transport-https
-#apt-get install apt-transport-https ca-certificates
-apt-get install -y apt-transport-https libcurl3-gnutls ca-certificates
-apt-get install -y mongodb-org
-systemctl start mongod
-systemctl enable mongod
+
+sudo apt-get install -y apt-transport-https libcurl3-gnutls ca-certificates
+
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongod
--- install_ruby.sh
+++ install_ruby.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "Start Install Ruby"
-apt update
-apt install -y ruby-full ruby-bundler build-essential
+sudo apt update
+sudo apt install -y ruby-full ruby-bundler build-essential
