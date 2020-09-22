288449f682f96bbc7e57f6c11af229932cb15855
Iliyev Telman <telman.devops@gmail.com>
2020-09-22 10:48:03 +0600

    Change README & mongo_sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -2,7 +2,9 @@
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 apt-get update
-apt-get install apt-transport-https ca-certificates
+#sudo apt-get install -y apt-transport-https
+#apt-get install apt-transport-https ca-certificates
+apt-get install -y apt-transport-https libcurl3-gnutls ca-certificates
 apt-get install -y mongodb-org
 systemctl start mongod
 systemctl enable mongod
--- install_ruby.sh
+++ install_ruby.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo ""
+echo "Start Install Ruby"
 apt update
 apt install -y ruby-full ruby-bundler build-essential
